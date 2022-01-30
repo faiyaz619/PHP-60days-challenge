10390840abd387fd75837a4b699fc046142a7e03
faiyaz619 <faiyaz.fahim.ff@gmail.com>
2022-01-30 09:48:46 +0600

    new simple php project

---
+++ class-10(php)/app/classes/Example.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace App\classes;
+
+
+class Example
+{
+    public $name;
+    public $email;
+    public $mobile;
+
+
+    public function index()
+    {
+        echo 'Hello World';
+    }
+}
